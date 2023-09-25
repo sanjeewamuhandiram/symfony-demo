a470a910baee24c5c8047e16217afa4d309e88b4
Sanj <39357333+sanjeewamadhuranga@users.noreply.github.com>
2023-09-26 00:18:33 +1300

    Update AddUserCommand.php

--- src/Command/AddUserCommand.php
+++ src/Command/AddUserCommand.php
@@ -7,7 +7,7 @@
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- * test 1
+ * test 2
  */
 
 namespace App\Command;
